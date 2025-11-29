559014e366dc0482277411310e7c696e1fdf15b7
Tulgar Dinç <47505611+tulgardinc@users.noreply.github.com>
2025-11-29 09:05:27 +0100

    navigating back

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Masonry, { MasonryItemData } from "../components/Masonry";
 import { useImages } from "../queries/useImages";
 import { ImageItem } from "../types";
 import { FullscreenImage } from "../components/FullscreenImage";
 import { AnimatePresence } from "framer-motion";
+import { useLocation, useNavigate, useParams } from "react-router";
 
 export default function Home() {
   const [selectedItem, setSelectedItem] = useState<ImageItem | null>(null);
   const [focusedItem, setFocusedItem] = useState<MasonryItemData | null>(null);
   const { data, isFetching, refetch } = useImages();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data) {
+      console.log(location.pathname);
+      const item = data.find(
+        (i) =>
+          i.id === location.pathname.substring(1, location.pathname.length - 1)
+      );
+      if (item) {
+        setSelectedItem(item);
+      } else {
+        setSelectedItem(null);
+      }
+    }
+  }, [location, data]);
+
+  const navigatgeBack = () => {
+    console.log("bruh");
+    navigate(-1);
+  };
 
   return (
     <main className="w-screen h-screen overflow-hidden">
@@ -29,12 +53,13 @@ export default function Home() {
             minItemWidth={300}
             selectedItem={selectedItem}
             onItemClick={(item) => {
-              setSelectedItem(item);
+              navigate(`/${item.id}/`);
             }}
             focusedItem={focusedItem}
             onItemFocus={(item) => {
               setFocusedItem(item);
             }}
+            navigateBack={navigatgeBack}
           />
         )}
       </div>
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,9 +2,11 @@ export type ImageData = {
   path: string;
   name: string;
   tags: string[];
+  id: string;
 };
 
 export type ImageItem = {
+  id: string;
   url: string;
   width: number;
   height: number;
